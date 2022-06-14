3389fa3c2f23feda200d2e5f03ddf391687e9fc5
FakharElyes <elyes.fakhar@esprit.tn>
2022-06-14 18:24:59 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import data from './constants/data';
 
 function App() {
+  const [random, setRandom] = useState(0);
+
+
+  useEffect(() => {
+    setRandom(Math.floor(Math.random() * data.listWords.length));
+  }, []);
+
+  console.log(random);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+      <div className="grid-item">1</div>
+      <div className="grid-item">2</div>
+      <div className="grid-item">3</div>
+      <div className="grid-item">4</div>
+      <div className="grid-item">5</div>
+      <div className="grid-item">6</div>
+      <div className="grid-item">7</div>
+      <div className="grid-item">8</div>
+      <div className="grid-item">9</div>
     </div>
   );
 }
